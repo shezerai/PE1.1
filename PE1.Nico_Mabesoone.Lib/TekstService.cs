21f42884057066c07b576a5debe5554a90425d7b
Unknown <kitysnorepoes@hotmail.com>
2019-04-01 13:02:21 +0200

    async methode afgewerkt

--- PE1.Nico_Mabesoone.Lib/TekstService.cs
+++ PE1.Nico_Mabesoone.Lib/TekstService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Net.Http;
 
 namespace PE1.Nico_Mabesoone.Lib
 {
@@ -22,7 +23,10 @@ namespace PE1.Nico_Mabesoone.Lib
             }
             else if(mode == TekstMode.Ascii)
             {
-
+                HttpClient eenClient = new HttpClient();
+                HttpResponseMessage eenResponse = new HttpResponseMessage();
+                 eenResponse= await eenClient.GetAsync("http://artii.herokuapp.com/make?text=" + input);
+                return await eenResponse.Content.ReadAsStringAsync();
             }
            
         }
